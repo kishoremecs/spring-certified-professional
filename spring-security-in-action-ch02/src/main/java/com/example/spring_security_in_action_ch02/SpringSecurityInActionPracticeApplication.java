9e79612ea018c95501b6a8c1c62c5b98838c7aa9
Kishore Kumar Rachuti <kmr.kishore@gmail.com>
2024-07-25 12:03:53 +0530

    Security Chapter 2 - Example - Basic Authentication

---
+++ spring-security-in-action-ch02/src/main/java/com/example/spring_security_in_action_ch02/SpringSecurityInActionPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_in_action_ch02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityInActionPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityInActionPracticeApplication.class, args);
+	}
+
+}
---
+++ spring-security-in-action-ch02/src/test/java/com/example/spring_security_in_action_ch02/SpringSecurityInActionPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_in_action_ch02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityInActionPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
